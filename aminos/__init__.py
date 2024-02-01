082e70ade8a597181c04a79bafa304b157d6ed7e
vcsidromeo <153546377+vcsidromeo@users.noreply.github.com>
2024-02-01 19:50:29 +0530

    Delete aminos directory

--- aminos/__init__.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-from .api import Wss
-from .src.objects import Event, Payload
-
-version = "1.0.6"
-newest = get("https://pypi.python.org/pypi/Amino-Socket/json").json()["info"]["version"]
-if version != newest:
-    print(f"\033[1;32mAmino-Socket New Version!: {newest} (Your Using {version})\033[1;0m")
